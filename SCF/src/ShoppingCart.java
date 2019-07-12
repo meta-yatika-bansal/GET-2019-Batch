8787e87ab6246ff12635025745d78755454106de
Yatika Bansal <yatika.bansal@metacube.com>
2019-07-12 18:10:46 +0530

    Assignment 1 10-July-2019

--- SCF/src/ShoppingCart.java
+++ SCF/src/ShoppingCart.java
@@ -6,6 +6,8 @@
  * Shoe priced at 1000(INR)
  * Belt priced at 500(INR)
  * Tie  priced at 650(INR)
+ * Bag  priced at 1250(INR)
+ * File priced at   50(INR)
  * Quantity of each item is assumed to be Unlimited.
  */
 
@@ -19,35 +21,40 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 /** This class is for the items in the stock */
-class Item{
-    public double price;                                           //price of each item
-    public String name;                                            //name of each item
+class Item{ 
+	//price of item
+    public double price;
+    //name of item
+    public String name;  
+    //Serial number of item
+    public int  serialNumber ;
     Item(){};
-    Item(String name, double price){                               //Parameterized constructor for initialization
+    Item(int serialNumber , String name, double price){                               
     this.price = price;
     this.name = name;
+    this.serialNumber = serialNumber;
     }
 }
 
 /** This class is for the cart*/
 class ShoppingCart{
-    List<Item> cart = new ArrayList<>();                           //Array list named cart to hold items
+    List<Item> cart = new ArrayList<>();                           
     
     //Method to add items in the cart
-    //Input is the item to be added and its Quantity
+    //@param is the item to be added and its Quantity
     void  addToCart(Item item , int quantity){                     
         for(int i =1; i<= quantity; i++){
-            cart.add(item);                                        //Predefined Method to add elements in array list 
+            cart.add(item);                                         
         }  
     System.out.println("Item Added");
     }
     
      //Method to delete items from the cart 
-     //Input is the item to be deleted
+     //@param is the item to be deleted
      void deleteFromCart(Item item){
-         int flag = 0;                                              //To identify whether item to be deleted exists 
+         int flag = 0;                                               
          for(Item i:cart){
-             if(i == item){                                          //To find the required item in cart list
+             if(i == item){                                          
                  flag = 1;
                  cart.remove(i);
                  System.out.println("Cart Updated!");
@@ -61,15 +68,16 @@ class ShoppingCart{
      
      //Method to show the contents of cart
      void showCart(){
-         List<Item> cartDistinct = (List<Item>) cart.stream().distinct().collect(Collectors.toList()); //Distinct items list
+         List<Item> cartDistinct = (List<Item>) cart.stream().distinct().collect(Collectors.toList()); 
          System.out.println("Name\tPrice\t\tQuantity");
          for(Item i : cartDistinct){
         	  int temp=Collections.frequency(cart,i);
-             System.out.println(i.name +"\t"+ (i.price)*temp +"INR" +"\t"+ temp);      //counts duplicates of a item
+             System.out.println(i.name +"\t"+ (i.price)*temp +"INR" +"\t"+ temp);      
          }
      } 
      
      //Method for billing
+     //returns the total price of the items in the cart
      double billing(){
          double sum = 0;                                               
          for(Item i:cart){
@@ -80,90 +88,93 @@ class ShoppingCart{
      
      public static void main(String args[]){
          ShoppingCart s = new ShoppingCart();
-         int quantity;                                                  //Quantity of each item to be added
-         int number;                                                    //Serial number of items
-         char addordelete;                                              
-         char yesorno;                                                  //Whether to repeat the loop
+         int quantity;                                                  
+         int number;                                                    
+         String addOrDelete;  
+         String lower;
+         int yesOrNo;                                                  
          Item choice = new Item();                                      
          Scanner sc = new Scanner(System.in);
+         Scanner sc1 = new Scanner(System.in);
 
-         Item item1,item2,item3;
-         item1 = new Item("Shoe",1000);
-         item2 = new Item("Belt",500);
-         item3 = new Item("Tie",650);
+         Item item1 = new Item(1,"Bag",1200);
+         Item item2 = new Item(2,"Belt",500);
+         Item item3 = new Item(3,"Tie",650);
+         Item item4 = new Item(4,"Shoe",1050);
+         Item item5 = new Item(5,"File",50);
 
          System.out.println("WELCOME");
-         System.out.println(" \nThe List of Items Available : ");         //The list of available stock
+         System.out.println(" \nThe List of Items Available : ");         
          System.out.println("Serial Number\t\tName\tPrice");
-         System.out.println("\t1\t\tShoe\t1000");
-         System.out.println("\t2\t\tBelt\t500");
-         System.out.println("\t3\t\tTie\t650");
-
+         System.out.println("\t"+item1.serialNumber+"\t\t"+item1.name +"\t" + item1.price);
+         System.out.println("\t"+item2.serialNumber+"\t\t"+item2.name +"\t" + item2.price);
+         System.out.println("\t"+item3.serialNumber+"\t\t"+item3.name +"\t" + item3.price);
+         System.out.println("\t"+item4.serialNumber+"\t\t"+item4.name +"\t" + item4.price);
+         System.out.println("\t"+item5.serialNumber+"\t\t"+item5.name +"\t" + item5.price);
+         
          do{
-	        System.out.println("\n To Add an item to your cart , Enter 'A' \n To Delete an item , Enter 'D' "
+	            System.out.println("\n To Add an item to your cart , Enter 'A' \n To Delete an item , Enter 'D' "
 	        		           + "\n To view the cart , Enter 'V'  \n For Billing , Enter 'B'");
-	        addordelete= sc.next().charAt(0);
-	        if(addordelete != 'A' && addordelete != 'a' && addordelete != 'v' && addordelete != 'V'
-	        && addordelete != 'd' && addordelete != 'D' && addordelete != 'b'&& addordelete != 'B' ){
-                System.out.println("WRONG !Please Enter correctly");
-                break;
-	        }
-	        if(addordelete == 'd' || addordelete == 'D' || addordelete == 'a' || addordelete == 'A'){
-	        	if(s.cart.isEmpty() && (addordelete == 'd' || addordelete == 'D') ){
-	        		System.out.println("Cart is empty!");
-	        		break;
-	        	}
-                System.out.println("Please Enter the serial number of the item");
-                try{
-                	number = sc.nextInt();
-                    if(number == 1){                                               //Converts the number entered to corresponding item
-                       choice = item1;
-                    }else if(number == 2){
-                       choice = item2;
-                    }else if(number == 3){
-                       choice = item3;
-                    }else{
-                       System.out.println("WRONG!Please enter correct serial number");
-                       break;
+	            addOrDelete= sc.nextLine();
+	            lower = addOrDelete.toLowerCase();
+	            if(!"a".equals(lower) && !"d".equals(lower) && !"v".equals(lower) && !"b".equals(lower)){
+                    System.out.println("WRONG !Please Enter correctly");
+                    break;
+	            }
+	            if("a".equals(lower) || "d".equals(lower)){
+	        	    if(s.cart.isEmpty() && "d".equals(lower)){
+	        		    System.out.println("Cart is empty!");
+	        		    break;
+	        	     }
+                    System.out.println("Please Enter the serial number of the item");
+                    try{
+                	    number = sc1.nextInt();
+                        if(number == 1){                                              
+                           choice = item1;
+                        }else if(number == 2){
+                           choice = item2;
+                        }else if(number == 3){
+                           choice = item3;
+                        }else{
+                           System.out.println("WRONG!Please enter correct serial number");
+                           break;
+                        }
+	                }
+                    catch(Exception e){
+             	       System.out.println("Please Enter a valid number");
+             	       break;
                     }
 	            }
-                catch(Exception e){
-             	   System.out.println("Please Enter a valid number");
-             	   break;
-                }
-	        }
-            switch(addordelete){
-                case 'A' :
-                case 'a' : System.out.println("\nEnter the Quantity");
-                           try{
-                        	   quantity = sc.nextInt();                                        //Quantity should be positive number
-                        	   if(quantity > 0){
-                        	       s.addToCart(choice,quantity);
-                                   break;
-                        	   }else{
-                        		   throw new Exception();
-                        	   }
-                           }
-                           catch(Exception e){
-                        	   System.out.println("Please Enter a valid number");
-                        	   break;
-                           }
-                case 'D' :
-                case 'd' :s.deleteFromCart(choice);
-                          break;
-                case 'V' :
-                case 'v' :System.out.println("\nItems in the cart :");
-                          s.showCart();
-                          break;
-                case 'B' :
-                case 'b' : System.out.println("\nTotal Bill : " + s.billing() +"INR");
-                           break;
-         }
+                switch(lower){
+                    case "a" : System.out.println("\nEnter the Quantity");
+                               try{
+                        	       quantity = sc1.nextInt();                                        
+                        	       if(quantity > 0){
+                        	           s.addToCart(choice,quantity);
+                                       break;
+                        	       }else{
+                        		       throw new Exception();
+                        	       }
+                                }
+                                catch(Exception e){
+                        	       System.out.println("Please Enter a valid number");
+                        	       break;
+                                }
+                    case "d" :s.deleteFromCart(choice);
+                              break;
+                    case "v" :System.out.println("\nItems in the cart :");
+                              s.showCart();
+                              break;
+                    case "b" : System.out.println("\nTotal Bill : " + s.billing() +"INR");
+                               break;
+                 }
             
-            System.out.println("\nDo You Want Anything Else?(Y/N)");                                   //prompt to repeat
-            yesorno= sc.next().charAt(0);
-            }while( yesorno == 'Y' || yesorno == 'y');
+                 System.out.println("\nDo You Want Anything Else?(1/0)");                                
+                 yesOrNo= sc1.nextInt();
+            }while( yesOrNo == 1);
+         
             System.out.println("\nTHANKYOU FOR SHOPPING");
             sc.close();
-          }
-}
+            sc1.close();
+    }
+  }
