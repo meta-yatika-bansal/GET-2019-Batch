ece6472b90e9c76961bc87d6ebe14847cc2d6762
meta-yatika-bansal <yatika.bansal@metacube.com>
2019-08-03 11:04:16 +0530

    Assignment 3 Question 3

---
+++ DSaAssignment3Question3/src/TestClass.java
@@ -0,0 +1,38 @@
+/*
+ * Test Class
+ */
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TestClass {
+	
+	int NUMBER = 5;
+	int QUOTA_VIRAT = 4;
+	Heap heap = new Heap(5);
+	
+	@Before
+	public void addTest(){
+		Bowler[] bowler = new Bowler[5];
+		bowler[0] = new Bowler("A", 1);
+		bowler[1] = new Bowler("B", 2);
+		bowler[2] = new Bowler("C", 3);
+		bowler[3] = new Bowler("D", 4);
+		bowler[4] = new Bowler("E", 1);
+		heap.add(bowler);
+	}
+	
+	@Test
+	public void getBowlerWithMaxRunTest(){
+		List<String> list = new ArrayList<String>();
+		list.add("D");
+		list.add("D");
+		list.add("C");
+		list.add("C");
+		assertEquals(list, heap.removeMax(QUOTA_VIRAT));
+	}
+}
