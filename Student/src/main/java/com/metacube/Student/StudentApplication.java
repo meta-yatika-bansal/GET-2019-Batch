6a78141a76ee0a08b093d6eca2edfef1fe24e39e
meta-yatika-bansal <yatika.bansal@metacube.com>
2019-09-12 18:02:24 +0530

    EAD-session11 assignment

---
+++ Student/src/main/java/com/metacube/Student/StudentApplication.java
@@ -0,0 +1,15 @@
+/*
+ * Main Class
+ */
+package com.metacube.Student;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentApplication.class, args);
+	}
+}
